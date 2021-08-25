0897ab2faddce6c58102b222c0ee24674b2f5b9c
bakapear <proxybomb@gmail.com>
2021-08-25 11:15:04 +0200

    math cmd fix

--- src/cmds/math.js
+++ src/cmds/math.js
@@ -9,7 +9,7 @@ module.exports = {
   usage: '<expression>',
   async exec (cmd, args) {
     let math = await doMath(args.join(' '))
-    return controller.party(math)
+    if (math) controller.party(math)
   }
 }
 
@@ -17,5 +17,5 @@ async function doMath (expression) {
   let body = await dp('https://api.mathjs.org/v4/', {
     query: { expr: expression }
   }).json().catch(e => e.body)
-  return body.toString()
+  return body ? body.toString() : null
 }
